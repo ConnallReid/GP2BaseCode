8104cfbcec27c7a141e30fd07f9ec537bbd646d2
Connall <connall@thereidfamily.co.uk>
2015-09-23 17:39:24 +0100

    Lab 1 Task 1 - 3 Complete

--- include/Common.h
+++ include/Common.h
@@ -2,20 +2,6 @@
 #define _COMMON_H
 
 #include <iostream>
-#include <string>
-#include <sstream>
-
-using namespace std;
-
-#include <GL/glew.h>
 #include <SDL.h>
-#include <SDL_opengl.h>
-
-
-#ifdef __APPLE__
-#include <OpenGL/glu.h>
-#elif WIN32
-#include <gl\GLU.h>
-#endif
 
 #endif
