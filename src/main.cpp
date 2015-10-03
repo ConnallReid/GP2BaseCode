b2297a682c79736092bda27ffa7951b8966b7e21
Connall <connall@thereidfamily.co.uk>
2015-10-03 22:42:32 +0100

    Lab 2 Task 5 - 7 Complete

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,156 @@
 #include "Graphics.h"
 #include "Vertex.h"
 
-Vertex verts[] = { { 0.0f, 1.5f, 0.0f, //Top x,y,z
-					 1.0f, 0.0f, 0.0f, 1.0f }, //r,g,b,a
+Vertex verts[] = {
+	//Front
+	{ -0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
 
-				   { -1.0f, 0.0f, 0.0f, //Bottom Left x,y,z
-					  0.0f, 1.0f, 0.0f, 1.0f }, //r,g,b,a
+	{ -0.5f, -0.5f, 0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f }, //Bottom Left
 
-				   { 1.0f, 0.0f, 0.0f, //Bottom Right x,y,z
-					 0.0f, 0.0f, 1.0f, 1.0f } }; //r,g,b,a
+	{ 0.5f, -0.5f, 0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+	{ 0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Right
+
+	//{ -0.5f, 0.5f, 0.5f,
+	//1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	//{ 0.5f, -0.5f, 0.5f,
+	//0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+//Back
+	{-0.5f, 0.5f, -0.5f,
+	1.0f,0.0f,1.0f,1.0f}, //Top Left
+
+	{-0.5f,-0.5f,-0.5f,
+	1.0f,1.0f,0.0f,1.0f}, //Bottom Left
+
+	{0.5f,-0.5f,-0.5f,
+	0.0f,1.0f,1.0f,1.0f}, //Bottom Right
+
+	{0.5f,0.5f,-0.5f,
+	1.0f,0.0f,0.0f,0.0f}, //Top Right
+
+	//{-0.5f,0.5f,-0.5f,
+	//1.0f,0.0f,1.0f,1.0f}, //Top Left
+
+	//{0.5f,-0.5f,-0.5f,
+	//0.0f,1.0f,1.0f,1.0f}, //Bottom Right
+
+/*
+//Top
+	{ -0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ 0.5f, 0.5f, 0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f }, //Bottom Left
+
+	{ -0.5f, 0.5f, 0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+	{ 0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 0.0f, 0.0f }, //Top Right
+
+	{ -0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ 0.5f, 0.5f, 0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+//Bottom
+	{ -0.5f, -0.5f, -0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ -0.5f, -0.5f, 0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f }, //Bottom Left
+
+	{ 0.5f, -0.5f, 0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+	{ 0.5f, -0.5f, -0.5f,
+	1.0f, 0.0f, 0.0f, 0.0f }, //Top Right
+
+	{ -0.5f, -0.5f, -0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ 0.5f, -0.5f, 0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+//Left
+	{ -0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ -0.5f, -0.5f, 0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f }, //Bottom Left
+
+	{ -0.5f, -0.5f, -0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+	{ -0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 0.0f, 0.0f }, //Top Right
+
+	{ -0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ -0.5f, -0.5f, -0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+//Right
+	{ 0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ 0.5f, -0.5f, 0.5f,
+	1.0f, 1.0f, 0.0f, 1.0f }, //Bottom Left
+
+	{ 0.5f, -0.5f, -0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+
+	{ 0.5f, 0.5f, -0.5f,
+	1.0f, 0.0f, 0.0f, 0.0f }, //Top Right
+
+	{ 0.5f, 0.5f, 0.5f,
+	1.0f, 0.0f, 1.0f, 1.0f }, //Top Left
+
+	{ 0.5f, -0.5f, -0.5f,
+	0.0f, 1.0f, 1.0f, 1.0f }, //Bottom Right
+*/
+};
+
+GLuint indices[] = {
+	//Front
+	0, 1, 2,
+	0, 3, 2,
+
+	//Left
+	4, 5, 1,
+	4, 1, 0,
+
+	//Right
+	3, 7, 2,
+	7, 6, 2,
+
+	//Bottom
+	1, 5, 2,
+	6, 2, 5,
+
+	//Top
+	4, 0, 7,
+	0, 7, 3,
+
+	//Back
+	4, 5, 6,
+	4, 7, 6
+};
 
 GLuint VBO;
+GLuint EBO;
 
 void cleanUp()
 {
+	glDeleteBuffers(1, &EBO);
 	glDeleteBuffers(1, &VBO);
 }
 
@@ -31,6 +168,13 @@ void initScene()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//Copy Vertex Data to VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	//Create buffer
+	glGenBuffers(1, &EBO);
+	//Make the EBO active
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//Copy index data to the EBO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void render()
@@ -43,6 +187,7 @@ void render()
 
 	//Make the new VBO actve. Repete here as a sanity check (may have changed since initialisation)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	//Establish its 3 coordinates per vertex with zero strides(space between elements) in array and contain floating point numbers
 	//The 3 parameter is now filled out, the pipeline needs to know the size of each vertex
@@ -59,15 +204,18 @@ void render()
     glMatrixMode( GL_MODELVIEW );
     //Reset using the Indentity Matrix
     glLoadIdentity( );
-    //Do translation, push the next bit of drawing 'back' 5 units
-    //on z-zaxis
-    //Everyting after this will be drawn at -5.0f on z-axis
-    //until reset by glLoadIdentity!
+    //Do translation, push the next bit of drawing 'back' 5 units on z-axis,
+    //Everyting after this will be drawn at -5.0f on z-axis until reset by glLoadIdentity!
+
+	gluLookAt(0.0, 0.0, 0.6, 0.0, 0.0, -1.0f, 0.0, 1.0, 0.0);
+
     glTranslatef( 0.0f, 0.0f, -6.0f );
 	//Actually draw the triangles, giving the number of vertices provided
-	glDrawArrays(GL_TRIANGLES, 0, sizeof(verts) / sizeof(Vertex));
+	glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 
     //Begin drawing triangles
+
+	/*
 	glBegin(GL_TRIANGLES);
 	     glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
 	     glVertex3f(-1.0f, 0.0f, 0.0f); // Top
@@ -75,12 +223,12 @@ void render()
 	     glVertex3f(0.0f, -1.5f, 0.0f); // Bottom Right
 	glEnd();
 
-    /* glBegin( GL_TRIANGLES );
+    glBegin( GL_TRIANGLES );
 		 glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
 		 glVertex3f(  0.0f,  1.5f, 0.0f ); // Top
 		 glVertex3f( -1.0f, 0.0f, 0.0f ); // Bottom Left
 	     glVertex3f(  1.0f, 0.0f, 0.0f ); // Bottom Right
-    glEnd( ); */
+    glEnd( ); 
 
 	glBegin(GL_TRIANGLES);
 		 glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
@@ -88,7 +236,7 @@ void render()
 	     glVertex3f(0.0f, -1.5f, 0.0f); // Bottom Left
 	     glVertex3f(2.0f, -1.5f, 0.0f); // Bottom Right
 	glEnd();
-
+	*/
 }
 
 int main(int argc, char * arg[])
